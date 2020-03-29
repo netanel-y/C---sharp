c740dfde393f3f6b7644a571866bcbc029dd0500
netanel-y <62289538+netanel-y@users.noreply.github.com>
2020-03-29 18:34:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("Enter length of array: ");
+
+            // length of array: 
+            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+
+
+            // number to multiply cells. 
+            Console.WriteLine("Enter number to multiply: ");
+            int NumToMul = Convert.ToInt32(Console.ReadLine());
+
+
+            // initialize array: 
+            int[] arr = new int[lengthOfArray];
+
+            Console.WriteLine($"Num of elements: {lengthOfArray}, num to mul:{NumToMul}");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * NumToMul;
+                Console.Write(arr[i] + ", ");
+            }
+
+
+            /*
+             * OUTPUT:
+             * ----------------
+            Enter length of array:
+            4
+            Enter number to multiply:
+            3
+            Num of elements: 4, num to mul:3
+            0, 3, 6, 9,
+             
+             */
+        }
+    }
+}
